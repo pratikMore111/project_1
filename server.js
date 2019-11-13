c45d55f28046631c98ea6b4cf8c035ba648f6fce
pratik <pratik@gmail.com>
2019-11-13 15:05:47 +0530

    third insert

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get('/', (request, response) => {
     response.send('welcome to my application')
 })
 
+app.get('/', (request, response) => {
+    response.send('welcome to my application')
+})
+
 app.listen(4000, () => {
     console.log(`Server started on port 4000`);
 });
